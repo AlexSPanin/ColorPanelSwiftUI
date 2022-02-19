4e9db06167a6d3cb76d71bcc73acbc6cea1788f1
Alexander Panin <panin1970@gmail.com>
2022-02-19 09:50:17 +0300

    Initial Commit

---
+++ ColorPanelSwiftUI/ColorPanelSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPanelSwiftUIApp.swift
+//  ColorPanelSwiftUI
+//
+//  Created by Александр Панин on 19.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorPanelSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
